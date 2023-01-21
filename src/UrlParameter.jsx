b98fdc59552bc5372fb2e8107e745c2e419ea43d
okamura123 <119551754+okamura123@users.noreply.github.com>
2023-01-21 18:39:24 +0900

    a

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>urlParameterのページです</h1>
-      <p>パラメーターは{}</p>
+      <p>パラメーターは{id}</p>
+      <p>クエリパラメーターは{query.get("name")}</p>
     </div>
   );
 };
